328e668cea711906d9edcfdfa76418be9f6a0422
Divyanshu singh <divyanshusingh7060@gmail.com>
2022-06-06 20:06:58 +0530

    add two number entered by the user

--- lib/add_two_number_entered_by_the_user.dart
+++ lib/add_two_number_entered_by_the_user.dart
@@ -1,5 +1,10 @@
 import 'dart:math';
 import 'dart:io';
 main(){
-  print("hello world");
+    stdout.write("Enter the first number");
+    int a=int.parse(stdin.readLineSync()!);
+    stdout.write("Enter the second number");
+    int b=int.parse(stdin.readLineSync()!);
+    int c=a+b;
+    print("a+b = $c");
 }
