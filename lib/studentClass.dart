335ec775ce10d01a81c8c2347464d2b90db3e5a7
Divyanshu050303 <divyanshusingh7060@gmail.com>
2022-06-09 10:11:32 +0530

    Student Call

---
+++ lib/studentClass.dart
@@ -0,0 +1,11 @@
+class student{
+  void name(String name){
+    print("Hello $name");
+  }
+  void place(String s){
+    print("Oh! you are from $s");
+  }
+  void old(String age){
+    print("Age =>$age");
+  }
+}
